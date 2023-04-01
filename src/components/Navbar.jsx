7e175753de29d7f6063e2ea86726ac1d4c27075e
Serban681 <serbands20@gmail.com>
2023-04-02 01:43:49 +0300

    Added navbar styles

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -15,10 +15,10 @@ export default function Navbar() {
 
     return (
         <div className={styles.container}>
-            <h3 className="logo">This is the logo</h3>
+            <div className="logo"></div>
             <div>
-                <button onClick={goToAllUsers} className={styles.button}>All Users</button>
-                <button onClick={goToCreateUser}>Add user</button>
+                <button onClick={goToAllUsers} className={styles.btn}>All Users</button>
+                <button onClick={goToCreateUser} className={styles.btn}>Add user</button>
             </div>
         </div>
     )
